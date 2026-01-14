4cc2f07d91ce5258b55b7a0bc0d485bc485233ce
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2026-01-14 19:51:10 +0000

    finished chartest

--- src/test/java/game/characters/CharacterTest.java
+++ src/test/java/game/characters/CharacterTest.java
@@ -44,39 +44,3 @@ class CharacterTest {
     }
 
 }
-
-class CharacterTest {
-
-    @Test
-    void testPlayerinitialHP(){
-        Player player = new Player();
-        assertEquals(100, player.getHealth());
-    }
-    @Test
-    void testPlayerinitialATK(){
-        Player player = new Player ();
-        assertEquals(15, player.getAttack());
-    }
-    @Test
-    void testdmgTakenByPlayer(){
-        Player player = new Player ();
-        player.takeDamage(30);
-        assertEquals(70, player.getHealth());
-    }
-
-    @Test
-    void testHealing() {
-        Player player = new Player();
-        player.takeDamage(60);
-        player.heal(20);
-        assertEquals(60, player.getHealth());
-    }
-    @Test
-    void testEnemyspawn(){
-        Enemy enemy = new Enemy("Sentinel", 150, 25);
-        assertEquals("Sentinel", enemy.getName());
-        assertEquals(150, enemy.getHealth());
-        assertEquals(25, enemy.getAttack());
-    }
-
-}
